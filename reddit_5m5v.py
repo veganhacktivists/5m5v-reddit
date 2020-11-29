e481966ba67c2826dfe2228f381374ace6c9629e
suanchinyeo <chin.yeo@mail.utoronto.ca>
2020-11-29 12:14:45 -0500

    tests passed

--- reddit_5m5v.py
+++ reddit_5m5v.py
@@ -6,35 +6,66 @@ from datetime import datetime
 import random
 
 class Reddit5m5v:
-    def __init__(self, bot_name, database):
+    def __init__(self, bot_name, database, verbose=True, test=False):
         self.config = self._load_config()
         self.topics_to_scan = self.config["topics"]
 
         self.reddit = praw.Reddit(bot_name)
         self.database = database
-        self.database_cursor = database.cursor()
-        self.submission_table_name = None
-        self.topics_comments_table = None
+        self.database_cursor = database.cursor(buffered=True)
+        self.submission_table_name = "reddit"
+        self.topics_comments_table = "reddit_comments"
 
         self.displayed_submissions = []
+
+        self.verbose = verbose
+        self.test = test
+
+        if self.test:
+            self.time_limit = 60
+        else:
+            self.time_limit = 10
+
+    def display_table(self, table_name):
+        sql = f"select * from {table_name}"
+        self.database_cursor.execute(sql)
+
+        print(f"Printing {table_name}.")
+        for row in self.database_cursor.fetchall():
+            print(row)
+
+    def check_table_for_rows(self, table, submission):
+        sql = f"select * from {table} where submission_id = '{submission.id}'"
+        self.database_cursor.execute(sql)
+        rows = [i for i in self.database_cursor.fetchall()]
+        return len(rows) >= 0
 
     def new_submission(self, submission):
         votes = submission.score
         comments = submission.num_comments
-        title = submission.title
+        title = submission.title.replace("'", "\\'")
         subreddit = submission.subreddit.display_name
         submission_id = submission.id
         submission_link = submission.permalink
-        submission_text = submission.selftext
+        submission_text = submission.selftext.replace("'", "\\'")
+
+        if self.verbose:
+            print(f"Entering new submission from r/{subreddit}: {title}")
+
 
         sql = f"""
             INSERT INTO {self.submission_table_name} 
                 (submission_id, title, text, votes, comments, subreddit, link)
-                VALUES ({submission_id}, {title}, {submission_text}, {votes}, {comments}, {subreddit},
-            {submission_link});"""
+                VALUES ('{submission_id}', '{title}', '{submission_text}', {votes}, {comments}, '{subreddit}',
+            '{submission_link}');"""
+
 
         self.database_cursor.execute(sql)
         self.database.commit()
+
+        if self.verbose:
+            self.display_table(self.submission_table_name)
+            print(f"r/{subreddit}: {title} added to {self.submission_table_name} table.")
 
         submission_topics = self.get_submission_topics(submission)
 
@@ -42,25 +73,32 @@ class Reddit5m5v:
             self.write_new_topic(submission_id, submission_topics[topic], topic)
 
     def write_new_topic(self, submission_id, comments, topic):
+
+        if self.verbose:
+            print(f"Writing new topic: {topic}")
+
         if len(comments) > 0:
             random_comment = random.sample(comments, 1)
+            comment_body = random_comment.body.replace("'", "\\'")
             sql = f"""
             INSERT INTO {self.topics_comments_table} 
                 (submission_id, example_comment_id, topic, num_comments, comment_text, comment_link, votes)
             VALUES
-                ({submission_id}, {random_comment.id}, {topic}, {len(comments)}, 
-            {random_comment.body}, {self.get_comment_url(random_comment)}, {random_comment.score})
+                ('{submission_id}', '{random_comment.id}', '{topic}', {len(comments)}, 
+            '{comment_body}', '{self.get_comment_url(random_comment)}', {random_comment.score})
                             """
         else:
             sql = f"""
             INSERT INTO {self.topics_comments_table} 
                 (submission_id, example_comment_id, topic, num_comments, comment_text, comment_link, votes)
             VALUES
-                ({submission_id}, null, {topic}, {len(submission_topics[topic])}, 
+                ('{submission_id}', null, '{topic}', {len(comments)}, 
             null, null, null)
             """
         self.database_cursor.execute(sql)
         self.database.commit()
+
+        self.display_table(self.topics_comments_table)
 
     def get_comment_url(self, comment):
         """
@@ -79,52 +117,85 @@ class Reddit5m5v:
         comments = submission.num_comments
         submission_id = submission.id
 
+        if self.verbose:
+            print(f"Updating submission r/{submission.subreddit.display_name}: {submission.title}")
 
         sql = f"""
             UPDATE {self.submission_table_name} 
             SET 
                 votes = {votes}, 
-                comments = {comments},
-            WHERE submission_id = {submission_id};"""
+                comments = {comments}
+            WHERE submission_id = '{submission_id}'"""
 
         self.database_cursor.execute(sql)
+        self.database.commit()
+
+        if self.verbose:
+            self.display_table(self.submission_table_name)
+            print(f"Updated {self.submission_table_name} for r/{submission.subreddit.display_name}:"
+                  f" {submission.title}")
+
+        if self.verbose:
+            print(f"Updating topics for r/{submission.subreddit.display_name}: {submission.title}")
 
         submission_topics = self.get_submission_topics(submission)
 
-        sql = f"select distinct * from submission_to_topic where submission_id = {submission_id};"
+        sql = f"select distinct * from {self.topics_comments_table} where submission_id = '{submission_id}';"
+
         self.database_cursor.execute(sql)
+        self.database.commit()
+
         row = {}
         columns = [i[0] for i in self.database_cursor.description]
         for i in self.database_cursor.fetchall():
             for o in range(len(columns)):
                 row[columns[o]] = i[o]
+            print(row["topic"])
+            print(submission_topics)
 
             if row["example_comment_id"]:
                 comment = self.reddit.comment(row["example_comment_id"])
                 votes = comment.score
+
                 sql = f"""
                     UPDATE {self.topics_comments_table}
                     SET
                         votes = {votes},
                         num_comments = {len(submission_topics[row["topic"]])}
-                    WHERE submission_id = {submission_id}
-                          and topic = {row["topic"]}
-                          and comment_id = {row["example_comment_id"]};"""
-            elif len(submission_topics[row["topics"]]):
-                random_comment = random(submission_topics[row["topics"]], 1)
+                    WHERE submission_id = '{submission_id}'
+                          and topic = '{row["topic"]}'
+                          and comment_id = '{row["example_comment_id"]}';"""
+
+                self.database_cursor.execute(sql)
+                self.database.commit()
+
+                if self.verbose:
+                    print(f"Updated {row['topic']}'s comment stats.")
+
+
+            elif len(submission_topics[row["topic"]]):
+                random_comment = random(submission_topics[row["topic"]], 1)
+
                 sql = f"""
                     UPDATE {self.topics_comments_table}
                     SET
-                        example_comment_id = {random_comment.id},
+                        example_comment_id = '{random_comment.id}',
                         num_comments = {len(submission_topics[row["topics"]])},
-                        comment_text = {random_comment.body},
-                        comment_link = {self.get_comment_url(random_comment)},
+                        comment_text = '{random_comment.body}',
+                        comment_link = '{self.get_comment_url(random_comment)}',
                         votes = {random_comment.score}
                     WHERE
-                        submission_id = {submission_id}
-                        and topic = {row["topic"]}
+                        submission_id = '{submission_id}'
+                        and topic = '{row["topic"]}'
                         """
-            del submission_topics[row["topics"]]
+
+                self.database_cursor.execute(sql)
+                self.database.commit()
+
+                if self.verbose:
+                    print(f"Added new example comment for {row['topic']}.")
+                    self.display_table(self.topics_comments_table)
+            del submission_topics[row["topic"]]
         for t in submission_topics:
             self.write_new_topic(submission_id, submission_topics[t], t)
 
@@ -135,7 +206,8 @@ class Reddit5m5v:
             result = self.topic_scan(submission.selftext + submission.title, t)
             if result:
                 submission_topics[t] = []
-
+        if self.verbose:
+            print(f"Scanned {submission.selftext + submission.title} found {[i for i in submission_topics]}.")
         return submission_topics
 
     def get_submission_topics(self, submission):
@@ -151,7 +223,7 @@ class Reddit5m5v:
 
         return submission_topics
 
-    def calculate_time_difference(timestamp):
+    def calculate_time_difference(self, timestamp):
         """
         given timestamp (UTC) calculate the time difference between the timestamp and right now.
         :param timestamp: UTC timestamp
@@ -159,11 +231,11 @@ class Reddit5m5v:
         """
         current_time = datetime.utcnow()
         time_difference = current_time - datetime.utcfromtimestamp(timestamp)
-        return time_difference.seconds / 60
+        return time_difference.seconds / self.time_limit
 
     def _load_config(self):
         with open("config.yaml") as f:
-            config = yaml.load(f)
+            config = yaml.safe_load(f)
         return config
 
     def topic_scan(self, text, topic):
@@ -192,35 +264,54 @@ class Reddit5m5v:
         for table in [self.submission_table_name, self.topics_comments_table]:
             sql = f"""  
                 DELETE from {table}
-                where submission_id = {submission.id}"""
+                where submission_id = '{submission.id}'"""
             self.database_cursor.execute(sql)
             self.database.commit()
+            if self.verbose:
+                print(f"Deleted {submission.id} from {table}.")
 
     def main(self):
-        subreddits = config["subreddit_list"]
+        subreddits = self.config["subreddit_list"]
         subreddits = self.reddit.subreddit("+".join(subreddits))
+
+        sql = f"select distinct submission_id from {self.submission_table_name};"
+        self.database_cursor.execute(sql)
+        logged_submissions = [i[0] for i in self.database_cursor.fetchall()]
+
         displayed_submissions = []
+        n = 200
         for submission in subreddits.stream.submissions():
             # check submission
+            print(logged_submissions)
             submission_topics = self.check_submission_topics(submission)
+
+
             if submission_topics:
                 # if valid, add to list
-                displayed_submissions.append(submission)
+                if submission.id not in logged_submissions:
+                    self.new_submission(submission)
+                sql = f"select distinct submission_id from {self.submission_table_name}"
+                self.database_cursor.execute(sql)
+                logged_submissions = [i[0] for i in self.database_cursor.fetchall()]
 
-            sql = f"select distinct submission_id from {self.submission_table_name};"
-            self.database_cursor.execute(sql)
-            logged_submissions = [i for i in self.database_cursor.fetchall()]
-
-            for s in displayed_submissions.copy():
-                time_difference = self.calculate_time_difference(s.created_utc)
+            for s in logged_submissions.copy():
+                displayed_submission = self.reddit.submission(id=s)
+                time_difference = self.calculate_time_difference(displayed_submission.created_utc)
                 if time_difference >= 10:
                     # delete old records
-                    self.delete_record(s)
-                    displayed_submissions.pop(0)
+                    self.delete_record(displayed_submission)
                 elif s in logged_submissions:
-                    self.update_submission(s)
+                    self.update_submission(displayed_submission)
                 elif s not in logged_submissions:
-                    self.new_submission(s)
+                    self.new_submission(displayed_submission)
+
+                sql = f"select distinct submission_id from {self.submission_table_name}"
+                self.database_cursor.execute(sql)
+                logged_submissions = [i[0] for i in self.database_cursor.fetchall()]
+            if self.test:
+                n -= 1
+                if n == 0:
+                    break
 
 
 if __name__ == "__main__":
---
+++ test.py
@@ -0,0 +1,36 @@
+import unittest
+import mysql.connector
+import yaml
+from reddit_5m5v import Reddit5m5v
+
+
+class MyTestCase(unittest.TestCase):
+    with open("config.yaml", "rb") as f:
+        config = yaml.safe_load(f)
+
+    mydb = mysql.connector.connect(
+        host=config["mysql_host"],
+        user=config["mysql_user"],
+        password=config["mysql_password"],
+        database=config["mysql_database"])
+
+    cursor = mydb.cursor()
+
+    def display_table(self, table_name):
+        sql = f"select * from {table_name};"
+        MyTestCase.cursor.execute(sql)
+        for row in MyTestCase.cursor.fetchall():
+            print(row)
+
+    def test_run(self):
+        reddit = Reddit5m5v("veganactivismbot", MyTestCase.mydb, test=True)
+        reddit.main()
+        self.display_table("reddit")
+        self.display_table("reddit_comments")
+
+    def test_display(self):
+        self.display_table("reddit")
+        self.display_table("reddit_comments")
+
+if __name__ == '__main__':
+    unittest.main()
